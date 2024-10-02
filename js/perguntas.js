0f9777ca2059af7d8a759a252dfb491375892d85
adrianapahola <129787720+adrianapahola@users.noreply.github.com>
2024-10-02 15:47:18 -0300

    adrii

---
+++ js/perguntas.js
@@ -0,0 +1,110 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que as duas sairam no recreio, a Sofia chamou a Marina para conversar. O que a Sofia queria falar?",
+        alternativas: [
+            {
+                texto: "Vamos se resolver",
+                afirmacao: [
+                    "Agora as duas voltaram a ser amigas ",
+                    "Agora as duas vão ter que ficar sozinhas no recreio, porque ninguem da escola gostam delas"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "vamos terminar com nossa amizade",
+                afirmacao: [
+                    "As duas ficaram falando mal uma da outra",
+                    "elas tiveram que voltar a falar porque não sabiam estar uma sem a outra"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Continuando a caminhada de volta para casa após você atravessar o bairro onde prefeito da cidade vive, você se depara com o bairro esquecido pelo prefeito. Não há asfalto, iluminação, apenas uma trilha submersa em um rio (embora não haja rios pela área, essa água parou aí de algum modo)...",
+        alternativas: [
+            {
+                texto: "Atravessar mesmo assim (fazendo Parkour)",
+                afirmacao: [
+                    "Atravessando o bairro que sequer foi inaugurado com um nome, você pula sobre as tábuas que foram usadas para cobrir a água, quando estava para sair você acidentalmente pisa numa poça de água que pela escuridão não viu, instantaneamente você sente a má energia da poça, corra para um UPA próximo (boa sorte)",
+                    "Fazendo Parkour nas casinhas do bairro esquecido, você pisa numa telha solta e cai dentro da casinha. Lá estava o homem do qual você chama de pai com sua segunda familia, após o choque você recebe alguns trocados para se calar, dinheiro que pode ser usado para investir em organizações de cunho social.",
+                    "Ante mesmo de conseguir correr para se desviar da água, devido a escuridão e a rua com buracos, você tropeça e cai, deixando cair alguns trocados que em breve alguém que vive ali pode encontrar, abrindo portas para um futuro brihante a essa pessoa que pode usar o dinheiro para melhorar de vida ou então gastar com pinga."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Reportar a situação para o prefeito.",
+                afirmacao: [
+                    "Vendo a situação e como aquilo podia prejudicar os moradores, você reporta ao prefeito que acaba por ignorar o pedido, inconformado você abre uma abertura contra o mesmo e no final consegue ceder a casa do prefeito para os moradores daquele bairro. A desigualdade pela cidade cai para 0,001% (sendo o prefeito).",
+                    "Após reportar de maneira sensata a situação ao prefeito, o bairro passa por uma transforção onde todos os moradores são retirados de suas casa e por fim apartamentos são feitos para acomodar mais pessoas, mas eles não foram feito exatamente para os antigos moradores viverem (parabéns, acabou com familias)",
+                    "Depois de reportar, o prefeito concordou em restaurar o bairro e agora além de estar iluminado e asfaltado, o bairro tem um nome."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Após atravessar o bairro você encontra um grupo promovendo ações sociais (supostamente)",
+        alternativas: [
+            {
+                texto: "Me junto a eles",
+                afirmacao: [
+                    "Se juntando ao grupo que promovia ações sociais você se sente comovido e expande as ações usando o pouco de pensão paga pelo seu pai, mas então percebe que pode receber bem mais se ele pagasse por seus últimos 10 anos de vida, abrindo um processo você recupera o valor e abre um movimento contra negadores de pensões aos filhos, dando mais condições a esses que nunca receberam pensão e falindo seus pais (parabéns, o numero de filhos sem pensões era antes maior que o numero de pais que deviam, logo o valor inverte e a taxa de desigualdade abaixa, permanecendo apenas os pais)",
+                    "Era um movimento promovendo ações para ajudar aspirantes a politicos, mas então você se recorda do atual prefeito e do bairro esquecido, foi assim que o prefeito ocupou o atual cargo, com isso você desvia as arrecadações desse grupos e investe.",
+                    "Se juntando ao grupo que promovia ações você se sente realizado, mas falha ao perceber que o grupo ajudava tiktokers a crescer, apoiavam npc's e diagnosticava gratuitamente outras pessoas com base em videos do tiktok (já pode terminar aqui o desafio e ir pro tiktok)."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Paro para observar e vou embora.",
+                afirmacao: [
+                    "Ao observar o grupo você percebe que são tiktokers trazendo mendigos para realizarem desafios em videos e então recebe uma recompensa misteriosa, o que não esperavam é que um desses mendigos que esperavam ajudar na verdade era o grande influenciador mendigo Bluezão, fazendo um teste social, ele te vê depois e te entrega parte de suas propriedades que podem ser usadas para criar residencias voluntárias. ",
+                    "O grupo que você parou para olhar, é um grupo que promovia ações para arrecadar fundos para manter os próximos CBLOL, estavam infelizes pois acabou, inspirado por você não ser um deles você vai para casa feliz já que você tem emprego e colabora com ONG's ",
+                    "Aquele grupo que você parrou para observar, na verdade era apenas a Virginia e companhia tentando vender rifa após o tigrinho levar suas propriedades"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Quando você finalmente chegava na rua de sua casa, você é parado pelo mendigo Bluezão, você tem duas escolhas a fazer sabendo que pode mudar sua vida.",
+        alternativas: [
+            {
+                texto: "Paro para falar com ele",
+                afirmacao: [
+                    " Ao parar para falar com Bluezão você é pego pela história de vida dele, nada muito comovente, mas você foi o primeiro que parou para o escutar, e no final você descobre que é um teste social, a herança dele é colocada em seu nome e assim você decide voluntariamente (ou não) doar para a caridade",
+                    "Parando para falar com Bluezão vocês entram em um consenso após entrar em um crise existencial, vocês elaboraram acidentalmente um bom plano para ajudar bairros necessitados e pessoas"
+                ],
+                proxima: 6,
+            },
+            {
+                texto: "Humilhar ele e ir embora",
+                afirmacao: [
+                    "Humilhando o BLuezão você o fez chorar, não man, não é assim que se contribui com a sociedade, você não teve sucesso com o seu objetivo de resolver os problemas do mundo.",
+                    "Humilhando o Bluezão ele joga umas moedas para que você deixasse ele, com essas moedas você pode investir já que se recorda de um video no tiktok onde um mano cresceu com uma moeda de um real, caso contrário você poderia simplesmente usar a moeda em maquinas de pegar Pokémon"
+                ],
+                proxima: 5,
+            },
+        ]
+    },
+    {
+        enunciado: "Depois de toda a jornada para voltar para casa, você finalmente chega e é recebido por sua mãe. Com a chegada do jantar ela olha para você: Não deixe comida no prato, tem gente passando fome",
+        alternativas: [
+            {
+                texto: "Tento rebater.",
+                afirmacao: [
+                    "Durante a discussão no jantar com sua mãe você rebate: Se eu jogar a commida fora tem mais chances da comida chegar neles do que quando como, mãe. Sua mãe reflete e concorda, com o passar dos anos isso virou conhecimentos de todos e no final essa era a solução da fome.",
+                    "Na discussão no jantar com sua mãe, você tentou rebater: Mãe, a senhora não me entende, ninguém me entende!. No final era apenas mais um surto adolescente, sua mãe segue com o jantar colaborando mais para a humanidade do que você colaborou durante o dia"
+                ],
+            },
+            {
+                texto: "Propor uma proposta de intervenção a fome",
+                afirmacao: [
+                    "Durante o jantar com sua mãe, você pensou apenas em si próprio, e a sua proposta de intervenção aglobava somente a sua fome, com isso você apenas termina o jantar e sobe para o seu quarto jogar uma partida de LoL e então dormir.",
+                    "Sua solução para proposta durante o janta foi usar do salário do prefeito para manter as familias que mais precisavam de recursos pela cidade. Com o passar do tempo outras cidades e então estados usaram da mesma ideia, consecutivamente o próprio presidente teve que se desfazer do que tinha pela população.",
+                    "Sua solução não foi bem interver, você simplesmente não terminou o jantar com o discurso de que passaria fome também como forma de conscientização e que sua mãe deveria fazer o mesmo e conscientizar os outros a fazer o mesmo assim que ela tentou te chamar de louco."
+                ],
+            },
+        ]
+    }
+];
